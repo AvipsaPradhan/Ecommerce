5b251df22794cbea0f15cd7721fe4dad808974b4
AvipsaPradhan <96482352+AvipsaPradhan@users.noreply.github.com>
2024-05-24 11:33:44 +0530

    Update SearchProduct.js

--- frontend/src/pages/SearchProduct.js
+++ frontend/src/pages/SearchProduct.js
@@ -18,38 +18,9 @@ const SearchProduct = () => {
 
         setData(dataResponse.data);
 
-        // Update searchQuery for the current user
-        const currentUser = JSON.parse(localStorage.getItem('user')); // Assuming you store user information in localStorage
-        if (currentUser) {
-            await updateUserSearchQuery(currentUser._id, query.search);
-        }
+        
     };
 
-    const updateUserSearchQuery = async (userId, newSearchQuery) => {
-      try {
-          // Fetch current searchQuery of the user
-          const userResponse = await fetch(`/api/users/${userId}`);
-          const userData = await userResponse.json();
-          const oldSearchQuery = userData.searchQuery || [];
-  
-          // Append the new search query to the existing array
-          const updatedSearchQuery = [...oldSearchQuery, newSearchQuery];
-  
-          // Update user document with the updated search query
-          const response = await fetch(`/api/users/${userId}`, {
-              method: 'PATCH',
-              headers: {
-                  'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({ searchQuery: updatedSearchQuery })
-          });
-          const data = await response.json();
-          console.log('User search query updated successfully:', data);
-      } catch (error) {
-          console.error('Error updating user search query:', error);
-      }
-  };
-  
 
     useEffect(() => {
         fetchProduct();
